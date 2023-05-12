320c37b71e615b3622f744e83e3912534788c1d6
Miguel Ángel Durán <miduga@gmail.com>
2023-05-12 20:03:51 +0200

    Add code of landing tesla

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			sans: ['Gotham SSm A, sans-serif']
+		},
+		extend: {},
+	},
+	plugins: [],
+}
